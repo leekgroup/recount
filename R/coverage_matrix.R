f56216b740caf618028916004ac539140f37f5f4
lcolladotor <lcollado@jhsph.edu>
2016-05-31 16:16:16 -0400

    Added the coverage_matrix() and expressed_regions() functions

---
+++ R/coverage_matrix.R
@@ -0,0 +1,131 @@
+#' Given a set of regions for a chromosome, compute the coverage matrix for a
+#' given SRA study.
+#'
+#' Given a set of genomic regions as created by \link{expressed_regions}, this
+#' function computes the coverage matrix for a library size of 40 million 100 bp
+#' reads for a given SRA study.
+#'
+#' @inheritParams expressed_regions
+#' @param regions A \link[GenomicRanges]{GRanges-class} object with regions
+#' for \code{chr} for which to calculate the coverage matrix.
+#' @param bpparam A \link[BiocParallel]{BiocParallelParam-class} instance which
+#' will be used to calculate the coverage matrix in parallel. By default, 
+#' \link[BiocParallel]{SerialParam-class} will be used.
+#' @param verboseLoad If \code{TRUE} basic status updates for loading the data
+#' will be printed.
+#' 
+#'
+#' @return A matrix with one row per region and one column per sample. The
+#' numbers in the cells are the counts (number of reads, or fraction in some
+#' cases) overlapping the region.
+#'
+#' @author Leonardo Collado-Torres
+#' @export
+#'
+#' @seealso \link{download_study}, \link[derfinder]{findRegions},
+#' \link[derfinder]{railMatrix}
+#'
+#' @examples
+#' ## Define expressed regions for study DRP002835, chrY
+#' regions <- expressed_regions('DRP002835', 'chrY', cutoff = 5L, 
+#'     maxClusterGap = 3000L)
+#'
+#' ## Now calculate the coverage matrix for this study
+#' coverageMatrix <- coverage_matrix('DRP002835', 'chrY', regions)
+#'
+#' ## One row per region
+#' identical(length(regions), nrow(coverageMatrix))
+#'
+
+coverage_matrix <- function(project, chr, regions, chunksize = 1000, bpparam = NULL, outdir = NULL, chrlen = NULL, verbose = TRUE, verboseLoad = verbose, ...) {
+    
+    ## Check inputs
+    stopifnot(is.character(project) & length(project) == 1)
+    stopifnot(is.character(chr) & length(chr) == 1)
+    
+    ## Use table from the package
+    url_table <- recount::recount_url
+    
+    ## Subset url data
+    url_table <- url_table[url_table$project == project, ]
+    stopifnot(nrow(url_table) > 0)
+    
+    ## Find chromosome length if absent
+    if(is.null(chrlen)) {
+        chrinfo <- read.table('https://raw.githubusercontent.com/nellore/runs/master/gtex/hg38.sizes',
+            col.names = c('chr', 'size'), colClasses = c('character',
+            'integer'))
+        chrlen <- chrinfo$size[chrinfo$chr == chr]
+        stopifnot(length(chrlen) == 1)
+    }
+    
+    samples_i <- which(grepl('[.]bw$', url_table$file_name) & !grepl('mean',
+        url_table$file_name))
+    ## Check if data is present, otherwise download it
+    if(!is.null(outdir)) {
+        ## Check sample files
+        sampleFiles <- sapply(samples_i, function(i) {
+            file.path(outdir, 'bw', url_table$file_name[i])
+        })
+        if(any(!file.exists(sampleFiles))) {
+            download_study(project = project, type = 'samples', outdir = outdir,
+                download = TRUE, ...)
+        }
+        
+        ## Check phenotype data
+        phenoFile <- file.path(outdir, paste0(project, '.tsv'))
+        if(!file.exists(phenoFile)) {
+            download_study(project = project, type = 'phenotype',
+                outdir = outdir, download = TRUE, ...)
+        }
+    } else {
+        .load_install('RCurl')
+        sampleFiles <- sapply(
+            download_study(project = project, type = 'samples',
+            download = FALSE)
+            , rtracklayer:::expandURL)
+        phenoFile <- download_study(project = project, type = 'phenotype',
+            download = FALSE)
+    }
+        
+    ## Read pheno data
+    pheno <- read.table(phenoFile, header = TRUE, stringsAsFactors = FALSE)
+    
+    ## Get sample names
+    m <- match(url_table$file_name[samples_i], paste0(pheno$run, '.bw'))
+    names(sampleFiles) <- pheno$run[m]
+    
+    ## Define library size normalization factor
+    targetSize <- 40e6 * 100
+    totalMapped <- pheno$auc[m]
+    mappedPerXM <- totalMapped / targetSize
+    
+    ## Load required packages
+    .load_install('derfinder')
+    .load_install('GenomicRanges')
+    .load_install('BiocParallel')
+    
+    ## Split regions into chunks
+    nChunks <- length(regions) %/% chunksize
+    if(length(regions) %% chunksize > 0) nChunks <- nChunks + 1
+    
+    ## Split regions into chunks
+    if(nChunks == 1) {
+        regs_split <- list(regions)
+    } else {
+        regs_split <- split(regions, cut(seq_len(length(regions)),
+            breaks = nChunks, labels = FALSE))
+    }
+    
+    ## Define bpparam
+    if(is.null(bpparam)) bpparam <- SerialParam()
+    
+    ## Load coverage data
+    resChunks <- lapply(regs_split, derfinder:::.railMatChrRegion, sampleFiles = sampleFiles, chr = chr, mappedPerXM = mappedPerXM, L = 1, verbose = verbose, BPPARAM.railChr = bpparam, verboseLoad = verboseLoad, chrlen = chrlen)
+    
+    ## Group results from chunks
+    coverageMatrix <- do.call(rbind, lapply(resChunks, '[[', 'coverageMatrix'))
+    
+    ## Finish
+    return(coverageMatrix)
+}
---
+++ R/expressed_regions.R
@@ -0,0 +1,111 @@
+#' Identify expressed regions from the mean coverage for a given SRA project
+#'
+#' This function uses the pre-computed mean coverage for a given SRA project to
+#' identify the expressed regions (ERs) for a given chromosome. It returns a
+#' \link[GenomicRanges]{GRanges-class} object with the expressed regions as
+#' defined by \link[derfinder]{findRegions}.
+#'
+#' @param project A character vector with one SRA study id.
+#' @param chr A character vector with the name of the chromosome.
+#' @param cutoff The base-pair level cutoff to use.
+#' @param outdir The destination directory for the downloaded file(s) that were
+#' previously downloaded with \link{download_study}. If the files are missing, 
+#' but \code{outdir} is specified, they will get downloaded first. By default
+#' \code{outdir} is set to \code{NULL} which will use the data from the web.
+#' We only recommend downloading the full data if you will use it several times.
+#' @param maxClusterGap This determines the maximum gap between candidate ERs.
+#' @param chrlen The chromosome length in base pairs. If it's \code{NULL}, the 
+#' chromosome length is extracted from the Rail-RNA runs GitHub repository.
+#' @param verbose If \code{TRUE} basic status updates will be printed along the 
+#' way.
+#' @param ... Additional arguments passed to \link{download_study} when
+#' \code{outdir} is specified but the required files are missing.
+#'
+#' @return A \link[GenomicRanges]{GRanges-class} object as created by
+#' \link[derfinder]{findRegions}.
+#'
+#' @author Leonardo Collado-Torres
+#' @export
+#'
+#' @seealso \link{download_study}, \link[derfinder]{findRegions},
+#' \link[derfinder]{railMatrix}
+#'
+#' @examples
+#' ## Define expressed regions for study SRP009615, chrY
+#' regions <- expressed_regions('SRP009615', 'chrY', cutoff = 5L, 
+#'     maxClusterGap = 3000L)
+#'
+#' \dontrun{
+#' ## Define the regions for multiple chrs
+#' regs <- sapply(chrs, expressed_regions, project = 'SRP009615', cutoff = 5L)
+#'
+#' ## You can then combine them into a single GRanges object if you want to
+#' library('GenomicRanges')
+#' single <- unlist(GRangesList(regs))
+#' }
+#'
+
+expressed_regions <- function(project, chr, cutoff, outdir = NULL,
+    maxClusterGap = 300L, chrlen = NULL, verbose = TRUE, ...) {
+    
+    ## Check inputs
+    stopifnot(is.character(project) & length(project) == 1)
+    stopifnot(is.character(chr) & length(chr) == 1)
+    
+    ## Use table from the package
+    url_table <- recount::recount_url
+    
+    ## Subset url data
+    url_table <- url_table[url_table$project == project, ]
+    stopifnot(nrow(url_table) > 0)
+    
+    ## Find chromosome length if absent
+    if(is.null(chrlen)) {
+        chrinfo <- read.table('https://raw.githubusercontent.com/nellore/runs/master/gtex/hg38.sizes',
+            col.names = c('chr', 'size'), colClasses = c('character',
+            'integer'))
+        chrlen <- chrinfo$size[chrinfo$chr == chr]
+        stopifnot(length(chrlen) == 1)
+    }
+        
+    ## Check if data is present, otherwise download it
+    if(!is.null(outdir)) {
+        ## Check mean file
+        meanFile <- file.path(outdir, 'bw', url_table$file_name[grep('mean',
+            url_table$file_name)])        
+        if(!file.exists(meanFile)) {
+            download_study(project = project, type = 'mean', outdir = outdir,
+                download = TRUE, ...)
+        }
+    } else {
+        .load_install('RCurl')
+        meanFile <- rtracklayer:::expandURL(
+            download_study(project = project, type = 'mean',
+            download = FALSE)
+            )
+    }
+    
+    ## Load required packages
+    .load_install('derfinder')
+    .load_install('GenomicRanges')
+    
+    ## Load coverage
+    meanCov <- loadCoverage(files = meanFile, chr = chr, chrlen = chrlen)
+    
+    ## Find regions
+    regs <- findRegions(position = Rle(TRUE, length(meanCov$coverage[[1]])),
+        fstats = meanCov$coverage[[1]], chr = chr,
+        maxClusterGap = maxClusterGap, cutoff = cutoff, verbose = verbose)
+    
+    ## If there are no regions, return NULL
+    if(is.null(regs)) regs <- GRanges()
+    
+    ## Format appropriately
+    names(regs) <- seq_len(length(regs))
+    
+    ## Set the length
+    seqlengths(regs) <- length(meanCov$coverage[[1]])
+    
+    ## Finish
+    return(regs)
+}
