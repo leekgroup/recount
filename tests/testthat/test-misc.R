be4d22f2eda9b812c018848cd1effc319b1c749b
lcolladotor <lcollado@jhsph.edu>
2016-06-01 16:13:12 -0400

    Add tests for geo_info()

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -4,3 +4,9 @@ test_that('Loading packages', {
     expect_equal(recount:::.load_install('recount'), NULL)
     expect_error(recount:::.load_install('somecrazyname'))
 })
+
+info <- geo_info('GSM836270')
+test_that('Geo info', {
+    expect_equal(info, geo_info('GSM836270', TRUE))
+    expect_equal(info$geo_accession, 'GSM836270')
+})
