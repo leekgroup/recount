1beb49d0d5f67126fd0bf8d4cbc3743c51419808
lcolladotor <lcollado@jhsph.edu>
2016-06-02 14:08:21 -0400

    Add geo_characteristics()

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -6,9 +6,12 @@ test_that('Loading packages', {
 })
 
 info <- geo_info('GSM836270')
+chars <- data.frame('cells' = 'K562', 'shrna.expression' = 'no',
+    'treatment' = 'Puromycin', stringsAsFactors = FALSE)
 test_that('Geo info', {
     expect_equal(info, geo_info('GSM836270', TRUE))
     expect_equal(info$geo_accession, 'GSM836270')
     expect_equal(find_geo('SRX110461'), 'GSM836270')
     expect_equal(find_geo(''), NA)
+    expect_equal(geo_characteristics(info), chars)
 })
