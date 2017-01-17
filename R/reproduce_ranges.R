16fd5d192c4d560a4c403824df7a64e19bc71cec
lcolladotor <lcollado@jhsph.edu>
2017-01-17 17:41:09 -0500

    v1.1.13 -- initial change to drop UCSC. Still have a few things to update like rse_gene_SRP009615

--- R/reproduce_ranges.R
+++ R/reproduce_ranges.R
@@ -3,11 +3,9 @@
 #' This function reproduces the gene or exon level information used for creating
 #' the \link[SummarizedExperiment]{RangedSummarizedExperiment-class}
 #' objects provided by recount. The annotation is based on
-#' \code{TxDb.Hsapiens.UCSC.hg38.knownGene} with the gene-level 
+#' Gencode v25 with the gene-level 
 #' information extracted with \code{genes()} (see 
-#' \link[GenomicFeatures]{transcripts} with default arguments. It can also be
-#' used to generate similar results with newer annotation or with alternative
-#' annotations for the human hg38 assembly.
+#' \link[GenomicFeatures]{transcripts} with default arguments.
 #' 
 #' @param level Either \code{genes} or \code{exon}. It specifies whether to
 #' return Gene or exon level information as a 
@@ -15,10 +13,11 @@
 #' \link[GenomicRanges]{GRangesList-class} object respectively. The gene level
 #' information contains the width of the reduced exons for that given gene
 #' which can be used to normalize the counts provided by recount.
-#' @param db Either \code{TxDb.Hsapiens.UCSC.hg38.knownGene} (default) or
+#' Can also be \code{both} in which case a 2 element list with the exon and the
+#' gene output is returned.
+#' @param db Either \code{Gencode.v25} (default) or
 #' \code{EnsDb.Hsapiens.v79}. The default option reproduces the annotation
-#' used when creating recount (when using TxDb.Hsapiens.UCSC.hg38.knownGene
-#' version 3.1.3) or updates it. EnsDb.Hsapiens.v79 can be used
+#' used when creating recount. EnsDb.Hsapiens.v79 can be used
 #' for an alternative annotation as showcased in the recount vignette.
 #'
 #' @return Either a \link[GenomicRanges]{GRanges-class} object like 
@@ -26,12 +25,10 @@
 #' like \link{recount_exons}.
 #'
 #' @details
-#' Note that the information used in recount was originally calculated with
-#' \url{https://github.com/leekgroup/recount-website/blob/master/genes/create_bed.R}
-#' with the versions as noted in
-#' \url{https://github.com/leekgroup/recount-website/blob/master/genes/logs/genes-hg38-ucsc.o1192789}.
-#' The actual package tarball for TxDb.Hsapiens.UCSC.hg38.knownGene we used is 
-#' available at \url{https://jhubiostatistics.shinyapps.io/recount/TxDb.Hsapiens.UCSC.hg38.knownGene_3.1.3.tar.gz}.
+#' 
+#' For Gencode.v25, we use the comprehensive gene annotation (regions: 
+#' \code{CHR}) from \url{https://www.gencodegenes.org/releases/25.html}
+#' (GRCh38.p7).
 #' 
 #' @author Leonardo Collado-Torres
 #' @export
@@ -54,25 +51,23 @@
 #' }
 #'
 
-reproduce_ranges <- function(level = 'gene',
-    db = 'TxDb.Hsapiens.UCSC.hg38.knownGene') {
+reproduce_ranges <- function(level = 'gene', db = 'Gencode.v25') {
     ## Check input
-    stopifnot(level %in% c('gene', 'exon'))
-    stopifnot(db %in% c('TxDb.Hsapiens.UCSC.hg38.knownGene',
-        'EnsDb.Hsapiens.v79'))
+    level <- tolower(level)
+    stopifnot(level %in% c('gene', 'exon', 'both'))
+    stopifnot(db %in% c('Gencode.v25', 'EnsDb.Hsapiens.v79'))
     
     
     
     ## Load required packages
     .load_install('GenomicFeatures')
-    if(db == 'TxDb.Hsapiens.UCSC.hg38.knownGene') {
-        .load_install('TxDb.Hsapiens.UCSC.hg38.knownGene')
-        txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene::TxDb.Hsapiens.UCSC.hg38.knownGene
-    } else {
+    if (db == 'Gencode.v25') {
+            txdb <- makeTxDbFromGFF('ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_25/gencode.v25.annotation.gff3.gz',
+            format = 'gff3', organism = 'Homo sapiens')
+    } else if(db == 'EnsDb.Hsapiens.v79') {
         .load_install('EnsDb.Hsapiens.v79')
         txdb <- EnsDb.Hsapiens.v79::EnsDb.Hsapiens.v79
-    } 
-    
+    }
 
     ## Get genes with default option single.strand.genes.only = TRUE
     genes <- GenomicFeatures::genes(txdb)
@@ -81,30 +76,36 @@ reproduce_ranges <- function(level = 'gene',
     exons <- GenomicFeatures::exonsBy(txdb, by = 'gene')
 
     ## Keep only exons for gene ids that we selected previously
-    exons <- exons[names(exons) %in% names(genes)]
-    
+    if(!all(names(exons) %in% names(genes))) {
+        warning('Dropping exons with gene ids not present in the gene list')
+        exons <- exons[names(exons) %in% names(genes)]
+    }
+        
     ## Reduce exons by gene so the exons won't be overlapping each other inside a gene
     exons <- GenomicRanges::reduce(exons)
+
+    if(level == 'exon') return(exons)
+        
+    ## For 'gene' or 'both', continue by:
+    ## * adding length of reduced exons by gene
+    genes$bp_length <- sum(GenomicRanges::width(exons))
     
-    if(level == 'exon') {
-        return(exons)
-    } else if(level == 'gene') {
-        ## Add length of reduced exons by gene
-        genes$bp_length <- sum(GenomicRanges::width(exons))
-        
-        ## Add gene symbol
-        .load_install('org.Hs.eg.db')
-        if(db == 'TxDb.Hsapiens.UCSC.hg38.knownGene') {
-            gene_info <- AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db,
-                names(genes), 'SYMBOL', 'ENTREZID')
-        } else {
-            gene_info <- AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db,
-                names(genes), 'SYMBOL', 'ENSEMBL', multiVals = 'CharacterList')
-        }
-        genes$symbol <- gene_info
-        
-        ## Done
+    ## * adding gene symbol
+    .load_install('org.Hs.eg.db')
+    if(db == 'Gencode.v25') {
+        gene_info <- AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db,
+            gsub('\\..*', '', names(genes)), 'SYMBOL', 'ENSEMBL',
+            multiVals = 'CharacterList')
+    } else if(db == 'EnsDb.Hsapiens.v79') {
+        gene_info <- AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db,
+            names(genes), 'SYMBOL', 'ENSEMBL', multiVals = 'CharacterList')
+    }
+    genes$symbol <- gene_info
+    
+    if(level == 'gene') {
         return(genes)
+    } else if (level == 'both') {
+        return(list('exon' = exons, 'gene' = genes))
     }
 }
 
